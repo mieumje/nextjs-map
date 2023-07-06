b6eea44dcd0555776aebf79ad160f2f722403255
mieumje <crewsade@naver.com>
2023-07-07 08:42:11 +0900

    feat: clientSideRendering(dynamic import)

---
+++ pages/section1/clientSideRendering.tsx
@@ -0,0 +1,32 @@
+import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+// import NoSSR from '../../components/section1/NoSSR';
+
+const NoSSR = dynamic(() => import('../../components/section1/NoSSR'), {
+  ssr: false,
+});
+
+const Example: NextPage = () => {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    const delayInSeconds = 2;
+
+    new Promise<number>((resolve) => {
+      setTimeout(() => resolve(Math.random()), delayInSeconds * 1000);
+    }).then((result) => setData(() => result));
+  }, []);
+
+  return (
+    <main>
+      <h1>clientSideRendering</h1>
+      <p>값 : {data}</p>
+
+      <h1>NoSSR</h1>
+      <NoSSR />
+    </main>
+  );
+};
+
+export default Example;
