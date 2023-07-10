cc2e0bb39c221a11abd4216908e1b99cc507941f
Park Min Je <mason@gridwiz.com>
2023-07-10 13:55:17 +0900

    feat: Header 컴포넌트 완성(props로 right elements 조건부 렌더링 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link';
 import styles from '../../styles/header.module.scss';
+import Image from 'next/image';
 
-type Props = {};
+type Props = {
+  rightElements?: React.ReactElement[];
+};
 
-const Header = ({}: Props) => {
+const Header = ({ rightElements }: Props) => {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
         <Link href="/" className={styles.box}>
-          <img
-            src="https://lecture-1.vercel.app/inflearn.png"
-            alt="inflean logo"
+          <Image
+            src="/inflearn.png"
             width={110}
-            height={40}
-            className={styles.box}
+            height={20}
+            alt="인프런 로고"
           />
         </Link>
       </div>
+      {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import Header from '@/components/common/Header';
+import styles from '@/styles/header.module.scss';
+import Link from 'next/link';
+import { VscFeedback } from 'react-icons/vsc';
+import { AiOutlineShareAlt } from 'react-icons/ai';
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header
+        rightElements={[
+          <button
+            onClick={() => {
+              alert('복사');
+            }}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link href="/feedback" className={styles.box} key="link">
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      />
     </>
   );
 }
